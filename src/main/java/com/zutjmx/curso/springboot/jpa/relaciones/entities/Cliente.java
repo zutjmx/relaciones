071294dd73623b7bffb2966f3a3e9cbc9b7e8b8a
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-08-12 10:21:45 -0600

    add Cliente and Factura entity classes

---
+++ src/main/java/com/zutjmx/curso/springboot/jpa/relaciones/entities/Cliente.java
@@ -0,0 +1,72 @@
+package com.zutjmx.curso.springboot.jpa.relaciones.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String paterno;
+    private String materno;
+    private String email;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, String paterno, String materno, String email) {
+        this.nombre = nombre;
+        this.paterno = paterno;
+        this.materno = materno;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaterno() {
+        return paterno;
+    }
+
+    public void setPaterno(String paterno) {
+        this.paterno = paterno;
+    }
+
+    public String getMaterno() {
+        return materno;
+    }
+
+    public void setMaterno(String materno) {
+        this.materno = materno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
---
+++ src/main/java/com/zutjmx/curso/springboot/jpa/relaciones/entities/Factura.java
@@ -0,0 +1,53 @@
+package com.zutjmx.curso.springboot.jpa.relaciones.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "facturas")
+public class Factura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descripcion;
+    
+    private Long total;
+
+    public Factura() {
+    }
+
+    public Factura(String descripcion, Long total) {
+        this.descripcion = descripcion;
+        this.total = total;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+}
